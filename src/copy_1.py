508599091a9b8b39de4295499c081f13ccd83e55
Maria de Fátima Nunes Alves <94319702+alvesmariadefatima@users.noreply.github.com>
2025-04-28 16:24:53 -0300

    Delete src/copy_1.py

--- src/copy_1.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-
-# Atualize com seus dados reais de acesso:
-DATABASE_URL = "postgresql+psycopg2://postgres:123456@localhost:5432/projeto_utcs_vital"
-
-engine = create_engine(DATABASE_URL)
